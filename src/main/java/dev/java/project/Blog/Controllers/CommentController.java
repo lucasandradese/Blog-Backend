77bae07574e4c7893a9e7bb776759497806205e5
lucasandradese <lucasandradese@outlook.com>
2025-11-14 13:02:58 -0300

    chore: Coloca lógica no CommentService e implementa rotas no controller

--- src/main/java/dev/java/project/Blog/Controllers/CommentController.java
+++ src/main/java/dev/java/project/Blog/Controllers/CommentController.java
@@ -1,33 +1,43 @@
 package dev.java.project.Blog.Controllers;
 
+import dev.java.project.Blog.Models.CommentModel;
+import dev.java.project.Blog.Services.CommentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/comment")
 public class CommentController {
 
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
     @GetMapping("/getAll")
-    public String getAll(){
-        return "getAll";
+    public List<CommentModel> getAll(){
+        return commentService.getAll();
     }
     @GetMapping("/get/{id}")
-    public String get(){
-        return "get";
+    public CommentModel get(@PathVariable Long id){
+        return commentService.getById(id);
     }
     @PostMapping("/save")
-    private String save(){
-        return "save";
+    private CommentModel save(@RequestBody CommentModel commentModel){
+        return commentService.save(commentModel);
     }
 
     @PatchMapping("/update/{id}")
-    private String updade(){
-        return "update";
+    private CommentModel updade(@PathVariable Long id, @RequestBody CommentModel commentModel){
+        return commentService.update(id, commentModel);
     }
 
     @DeleteMapping("delete/{id}")
-    private void delete(){
-
+    private void delete(@PathVariable Long id){
+        commentService.delete(id);
     }
 
 }
--- src/main/java/dev/java/project/Blog/Services/CommentService.java
+++ src/main/java/dev/java/project/Blog/Services/CommentService.java
@@ -1,8 +1,12 @@
 package dev.java.project.Blog.Services;
 
+import dev.java.project.Blog.Models.CommentModel;
 import dev.java.project.Blog.Repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -11,5 +15,29 @@ public class CommentService {
 
     public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
+    }
+
+    public List<CommentModel> getAll() {
+        return commentRepository.findAll();
+    }
+
+    public CommentModel getById(Long id) {
+        Optional<CommentModel> commentId = commentRepository.findById(id);
+        return commentId.orElse(null);
+    }
+
+    public CommentModel save(CommentModel commentModel) {
+        return commentRepository.save(commentModel);
+    }
+
+    public CommentModel update(Long id, CommentModel commentModel) {
+        if(commentRepository.existsById(id)){
+            commentRepository.save(commentModel);
+        }
+        return null;
+    }
+
+    public void delete(Long id) {
+        commentRepository.deleteById(id);
     }
 }
