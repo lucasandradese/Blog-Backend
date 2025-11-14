aed6fb8e17d604a015d64b29182bc38f44397494
lucasandradese <lucasandradese@outlook.com>
2025-11-14 12:38:38 -0300

    chore: Coloca lógica no TagServeces e implenta rotas no controller

--- src/main/java/dev/java/project/Blog/Controllers/TagController.java
+++ src/main/java/dev/java/project/Blog/Controllers/TagController.java
@@ -1,32 +1,41 @@
 package dev.java.project.Blog.Controllers;
 
-import org.springframework.stereotype.Controller;
+import dev.java.project.Blog.Models.TagModel;
+import dev.java.project.Blog.Services.TagService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/tag")
 public class TagController {
 
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
     @GetMapping("/getAll")
-    public String getAll(){
-        return "getAll";
+    public List<TagModel> getAll(){
+        return tagService.getAll();
     }
     @GetMapping("/get/{id}")
-    public String get(){
-        return "get";
+    public TagModel get(@PathVariable Long id){
+        return tagService.getById(id);
     }
     @PostMapping("/save")
-    private String save(){
-        return "save";
+    private TagModel save(@RequestBody TagModel tagModel){
+        return tagService.save(tagModel);
     }
 
     @PatchMapping("/update/{id}")
-    private String updade(){
-        return "update";
+    private TagModel updade(@PathVariable Long id, @RequestBody TagModel tagModel){
+        return tagService.update(id, tagModel);
     }
 
     @DeleteMapping("delete/{id}")
-    private void delete(){
-
+    private void delete(Long id){
+        tagService.delete(id);
     }
 }
--- src/main/java/dev/java/project/Blog/Services/TagService.java
+++ src/main/java/dev/java/project/Blog/Services/TagService.java
@@ -1,7 +1,11 @@
 package dev.java.project.Blog.Services;
 
+import dev.java.project.Blog.Models.TagModel;
 import dev.java.project.Blog.Repositories.TagRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TagService {
@@ -9,5 +13,29 @@ public class TagService {
 
     public TagService(TagRepository tagRepository) {
         this.tagRepository = tagRepository;
+    }
+
+    public TagModel save(TagModel tagModel){
+        return tagRepository.save(tagModel);
+    }
+
+    public List<TagModel> getAll(){
+        return tagRepository.findAll();
+    }
+
+    public TagModel getById(Long id) {
+        Optional<TagModel> tagId = tagRepository.findById(id);
+        return tagId.orElse(null);
+    }
+
+    public TagModel update(Long id, TagModel tagModel){
+        if(tagRepository.existsById(id)){
+            return tagRepository.save(tagModel);
+        }
+        return null;
+    }
+
+    public void delete(Long id){
+        tagRepository.deleteById(id);
     }
 }
