5f308dd823448eba8c71d5ce1c31bdca6a389268
lucasandradese <lucasandradese@outlook.com>
2025-11-14 12:52:30 -0300

    chore: Coloca lógica no PostService e implementa rotas no controller

--- src/main/java/dev/java/project/Blog/Controllers/PostController.java
+++ src/main/java/dev/java/project/Blog/Controllers/PostController.java
@@ -1,32 +1,42 @@
 package dev.java.project.Blog.Controllers;
 
+import dev.java.project.Blog.Models.PostModel;
+import dev.java.project.Blog.Services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/post")
 public class PostController {
 
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/getAll")
-    public String getAll(){
-        return "getAll";
+    public List<PostModel> getAll(){
+        return postService.getAll();
     }
     @GetMapping("/get/{id}")
-    public String get(){
-        return "get";
+    public PostModel get(@PathVariable Long id){
+        return postService.getById(id);
     }
     @PostMapping("/save")
-    private String save(){
-        return "save";
+    private PostModel save(@RequestBody PostModel postModel){
+        return postService.save(postModel);
     }
 
     @PatchMapping("/update/{id}")
-    private String updade(){
-        return "update";
+    private PostModel updade(@PathVariable Long id, PostModel postModel){
+        return postService.update(id, postModel);
     }
 
     @DeleteMapping("delete/{id}")
-    private void delete(){
-
+    private void delete(@PathVariable Long id){
+        postService.delete(id);
     }
 }
--- src/main/java/dev/java/project/Blog/Services/PostService.java
+++ src/main/java/dev/java/project/Blog/Services/PostService.java
@@ -1,8 +1,12 @@
 package dev.java.project.Blog.Services;
 
 
+import dev.java.project.Blog.Models.PostModel;
 import dev.java.project.Blog.Repositories.PostRepostory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -11,5 +15,30 @@ public class PostService {
 
     public PostService(PostRepostory postRepostory) {
         this.postRepostory = postRepostory;
+    }
+
+
+    public PostModel save(PostModel postModel) {
+        return postRepostory.save(postModel);
+    }
+
+    public List<PostModel> getAll() {
+        return postRepostory.findAll();
+    }
+
+    public PostModel getById(Long id) {
+        Optional<PostModel> postID = postRepostory.findById(id);
+        return postID.orElse(null);
+    }
+
+    public PostModel update(Long id, PostModel postModel) {
+        if(postRepostory.existsById(id)){
+            return postRepostory.save(postModel);
+        }
+        return null;
+    }
+
+    public void delete(Long id) {
+        postRepostory.deleteById(id);
     }
 }
